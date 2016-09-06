54d9e6f7d8ab5c1d15f23a8f3c3b1b8f79092f28
mjSurber <matthewsurber96@gmail.com>
2016-09-06 15:25:04 -0500

    Added in java code handling seekbar and new landscape layout.

--- app/src/main/java/com/surber/matthew/seekbar/SeekBarActivity.java
+++ app/src/main/java/com/surber/matthew/seekbar/SeekBarActivity.java
@@ -2,6 +2,8 @@ package com.surber.matthew.seekbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SeekBarActivity extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class SeekBarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seek_bar);
+
+        final TextView seekbarValueTV = (TextView) findViewById(R.id.seek_bar_value_textview);
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        int seekbarProgress = seekBar.getProgress();
+
+        seekbarValueTV.setText("The seekbar value is " + seekbarProgress);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekbarValueTV.setText("The seekbar value is  " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
+
 }
